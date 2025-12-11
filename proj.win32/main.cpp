d9a8aa35b3eb17c9301245cce2faa71317dfb3f7
Amazing！ <2724567451@qq.com>
2025-12-11 22:38:06 +0800

    Feature/yzh map (#18)
    * 修改.gitignore
    
    * 移除误提交的.vs文件夹
    
    * 实现地图基类SceneMap
    
    * 存储HomeVillage地图
    
    * 存储HomeVillage瓦片地图
    
    * 简单的登录界面
    
    * 初步地图和封面
    
    * 登录界面
    
    * 修补。
    
    * 解决冲突
    
    * 解决冲突
    
    * 更改CMakeLists.txt
    
    * 改bug

--- proj.win32/main.cpp
+++ proj.win32/main.cpp
@@ -23,10 +23,10 @@
  ****************************************************************************/
 
 #include "main.h"
-#include "AppDelegate.h"
+#include "Scene/AppDelegate.h"
 #include "cocos2d.h"
 
-USING_NS_CC;
+USING_NS_CC;  
 
 int WINAPI _tWinMain(HINSTANCE hInstance,
                        HINSTANCE hPrevInstance,
